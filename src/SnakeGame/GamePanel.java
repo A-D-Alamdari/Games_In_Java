57e9bf31cbb063a7f5fed70ab6cb6983b30f97be
Amin D. Alamdari <amin_deldari@hotmail.com>
2023-06-27 15:05:22 +0300

    Snake Game Design Part is Added!

---
+++ src/SnakeGame/GamePanel.java
@@ -0,0 +1,57 @@
+package SnakeGame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.random.*;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    public GamePanel() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+
+    public class MyKeyAdaptor extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+    }
+}
